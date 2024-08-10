8e5286f2014f7488e2ea4f4620f222a131548655
Benjamin Cardwell <bencardwell1999@gmail.com>
2024-08-10 14:42:09 -0400

    Exercise 3.2 complete

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+
+
+app.get('/info', (request, response) => {
+  response.contentType = "text/html"
+  response.send(`<div>Phonebook has info for ${persons.length} people <br/> ${new Date()} </div>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
