c3f22a49f604dba0a33bbe5314aaeaaf366d6586
Benjamin Cardwell <bencardwell1999@gmail.com>
2024-08-11 09:54:09 -0400

    Exercise 3.8 complete

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-const morgan = require('morgan')
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+//tiny format is  :method :url :status :res[content-length] - :response-time ms
 
 
 let persons = [
