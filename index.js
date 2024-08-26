f32fee9b7d69d073fb7ead283418a31121dedd38
Benjamin Cardwell <bencardwell1999@gmail.com>
2024-08-26 16:43:06 -0400

    Exercise 3.8 complete

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+// create 'body' token
 morgan.token('body', (req, res) => JSON.stringify(req.body))
+// use morgan and use body token, i.e.  :body
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
