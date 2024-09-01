3c3b7e8f14493ca47320651598f06d81527c6b3e
Benjamin Cardwell <bencardwell1999@gmail.com>
2024-09-01 13:39:42 -0400

    Replace index.js with the answer key, to test on Koyeb

--- index.js
+++ index.js
@@ -1,140 +1,112 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
 require('dotenv').config()
 
-app.use(express.static('dist'))
-
-const cors = require('cors')
 const morgan = require('morgan')
-const PersonModel = require('./models/person')
+morgan.token('body', (req) => JSON.stringify(req.body))
 
-// necessary for being able to deploy frontend with the backend, using the dist folder
-// static bc the frontend is built into a static index.html (which contains script ref to js)
-// it works like this: whenever Express gets an HTTP GET request,
-// it will first check if the dist directory contains a file corresponding to the request's address
-
-
-app.use(cors())
-app.use(express.json())
-
-morgan.token('body', (req, res) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.get('/api/persons', (request, response) => {
-  PersonModel.find({}).then(persons => {
-    response.json(persons)
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log('Connected to MongoDB')
   })
-  // response.json({msg: "hello"})
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  PersonModel.findById(request.params.id)
-    .then(result => {
-      if (result) {
-        response.json(result)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
-  // response.json({msg: "hello"})
-})
-
-app.get('/info', (request, response) => {
-  response.contentType = 'text/html'
-  PersonModel.find({}).then(result => {
-    response.send(`<div>Phonebook has info for ${result.length} people <br/> ${new Date()} </div>`)
-  })
-  // response.json({msg: "hello"})
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-  PersonModel.findByIdAndDelete(request.params.id)
-    .then(result => {
-      console.log(`deletion result: <${result}>`)
-      response.status(204).end()
-    })
-    .catch(error => {
-      console.log(`deletion error: ${error}`)
-      return next(error)
-    })
-  
-  // const id = request.params.id
-  // persons = persons.filter(person => person.id !== id)
-  // console.log(`persons after deletion of ${id}`, persons)
-  // response.status(204).end()
-  // response.json({msg: "hello"})
-})
-
-app.post('/api/persons', (request, response, next) => {
-  // console.log('persons before: ', persons)
-
-  const body = request.body
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name and number must be provided' 
-    })
-  }
-  // if (persons.find((p) => p.name === person.name)) {
-  //   return response.status(400).json({ 
-  //     error: 'name must be unique' 
-  //   })
-  // }
-
-  console.log('person to be added: ', body)
-  const person = new PersonModel({
-    name: body.name,
-    number: body.number
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error.message)
   })
 
-  person.save()
-    .then(result => {
-      console.log(`${person.name} saved`)
-      response.json(result)
-    })
-    .catch(error => next(error))
-  // response.json({msg: "hello"})
-})
+const Person = require('./models/person')
 
-app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-  PersonModel.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
-  // response.json({msg: "hello"})
-})
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+const errorHandler = (error, req, res, next) => {
+  console.log(error.name , error.name === 'ValidationError')
 
   if (error.name === 'CastError') {
-    return response.status(400).json({ error: 'malformatted id' })
-
+    return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    console.log('====')
+    return res.status(400).json({ error: error.message })
   }
 
   next(error)
 }
 
-app.use(unknownEndpoint)
-// handler of requests with result to errors
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
+
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(400).json({ error: 'name or number missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (req, res) => {
+  const date = new Date()
+  Person.countDocuments().then(count => {
+    res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+  })
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
